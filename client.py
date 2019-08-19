2dcf54419e637deab1dec9ab4f1bc6d657260dc5
Jeremiah Nofrada <jnofrada@thevaliantway.com>
2019-08-19 15:08:56 -0400

    info xchange

--- client.py
+++ client.py
@@ -7,13 +7,18 @@ HOST = '172.20.1.232'
 PORT = 5000
 
 def message_back(socket):
+    #serial = get_serial()
+    serial = "test serial"
+    print (serial)
     client = input("Which client are you? ")
-    client = get_buff(client)
-    socket.send(bytes(client, "utf-8"))
+    asset = set_asset()
+    msg = get_buff(f"Serial: {serial}\nClient: {client}\nAsset: {asset}")
+    socket.send(bytes(msg, "utf-8"))
     msg2 = ''
     full2 = ''
     new_msg = True
     while True:
+        print ("Waiting message back")
         msg2 = socket.recv(32)
         if new_msg:
             msg_len = int(msg2[:BUF_SIZE])
@@ -24,31 +29,46 @@ def message_back(socket):
             new_msg = True
             break
 
+def get_serial():
+    task = subprocess.Popen(['system_profiler', 'SPHardwareDataType'],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = task.communicate()
+    for l in out.split('\n'):
+        if 'Serial Number (system):' in l:
+            serial_line = l.strip()
+            break
+        serial = serial_line.split(' ')[-1]
+    return (serial)
+
+def set_asset():
+    asset = input("What is the asset tag: ")
+    #subprocess.call(["sudo", "scutil", "--set", "LocalHostName", asset])
+    #subprocess.call(["sudo", "scutil", "--set", "HostName", asset])
+    #subprocess.call(["sudo", "scutil", "--set", "ComputerName", asset])
+    return (asset)
+
 def get_buff(message):
     message = f"{len(message):<{BUF_SIZE}}" + message
     return (message)
 
 def loop():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    msg1 = ''
+    full1 = ''
+    new_msg = True
     while True:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print ("connecting")
-        s.connect((HOST, PORT))
-        msg1 = ''
-        full1 = ''
-        new_msg = True
-        print ("Waiting message")
-        while True:
-            msg1 = s.recv(32)
-            if new_msg:
-                msg_len = int(msg1[:BUF_SIZE])
-                new_msg = False
-            full1 += msg1.decode("utf-8")
-            if len(full1)-BUF_SIZE == msg_len:
-                print(full1[BUF_SIZE:])
-                new_msg = True
-                break
-        s.send(bytes("Hello Server", "utf-8"))
-        message_back(s)
+        msg1 = s.recv(32)
+        if new_msg:
+            msg_len = int(msg1[:BUF_SIZE])
+            new_msg = False
+        full1 += msg1.decode("utf-8")
+        if len(full1)-BUF_SIZE == msg_len:
+            print(full1[BUF_SIZE:])
+            new_msg = True
+            break
+    print("going to message")
+    message_back(s)
+    s.close()
 
 def main():
     loop()
